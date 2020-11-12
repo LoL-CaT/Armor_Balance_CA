d31d712d7beecc0db183066a92c69f10f2f9ba09
LoL-CaT <W304088@yandex.ru>
2020-11-12 07:39:58 +0700

    A25

--- resource/script/multiplayer/bot.data.purchase.standard.lua
+++ resource/script/multiplayer/bot.data.purchase.standard.lua
@@ -3,39 +3,78 @@ Purchases["standard"] = {
 		Repeat = 0,
 		Units = {
 			["rus"] = {--rus
-				--{priority = 2.0, class = UnitClass.Infantry, unit = "riflemans(rus)"},
-				{priority = 2.0, class = UnitClass.Infantry, unit = "home_guards(rus)"},
-				--{priority = 1.0, class = UnitClass.Infantry, unit = "smgs(rus)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "elites(rus)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "stormtroopers(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "home_guards(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "smgs(rus)"},
+				{priority = 3.0, class = UnitClass.Infantry, unit = "riflemans(rus)"},
+				{priority = 4.0, class = UnitClass.Infantry, unit = "elites(rus)"},
+				{priority = 5.0, class = UnitClass.Infantry, unit = "stormtroopers(rus)"},
+				{priority = 10.0, class = UnitClass.Tank, unit = "m24_chaffee"},				
 			},
 			["ger"] = {--ger
-				--{priority = 2.0, class = UnitClass.Infantry, unit = "riflemans(ger)"},
-				{priority = 2.0, class = UnitClass.Infantry, unit = "home_guards(ger)"},
-				--{priority = 1.0, class = UnitClass.Infantry, unit = "smgs(ger)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "elites(ger)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "stormtroopers(ger)"},
+				{priority = 1.4, class = UnitClass.Infantry, unit = "home_guards(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "smgs(ger)"},
+				{priority = 1.6, class = UnitClass.Infantry, unit = "riflemans(ger)"},
+				{priority = 1.7, class = UnitClass.Infantry, unit = "elites(ger)"},
+				{priority = 1.8, class = UnitClass.Infantry, unit = "stormtroopers(ger)"},
+--;°°°Medium tanks°°°				
+				{priority = 1.4, class = UnitClass.Tank, unit = "pz3f"},--39
+				{priority = 1.4, class = UnitClass.Tank, unit = "d2"},--39
+				{priority = 1.4, class = UnitClass.Tank, unit = "s35"},--39
+				{priority = 1.4, class = UnitClass.Tank, unit = "pz4d"},--39
+				{priority = 1.5, class = UnitClass.Tank, unit = "pz4e"},--40
+				{priority = 1.5, class = UnitClass.Tank, unit = "pz3g"},--40
+				{priority = 1.6, class = UnitClass.Tank, unit = "pz3"},--41
+				{priority = 1.6, class = UnitClass.Tank, unit = "pz4f_U"},--41
+				{priority = 1.7, class = UnitClass.Tank, unit = "pz3l"},--42
+				{priority = 1.7, class = UnitClass.Tank, unit = "pz4g"},--42
+				{priority = 1.8, class = UnitClass.Tank, unit = "pz4h"},--43
+				{priority = 1.8, class = UnitClass.Tank, unit = "pz4j"},--44
+				{priority = 1.9, class = UnitClass.Tank, unit = "pz5d"},--43
+				{priority = 1.9, class = UnitClass.Tank, unit = "pz5a"},--44
+				{priority = 1.9, class = UnitClass.Tank, unit = "pz5g"},--44
+				{priority = 2.0, class = UnitClass.Tank, unit = "panther_ii_88mm"},--45
+				{priority = 2.1, class = UnitClass.Tank, unit = "e-50_75l100"},--46
+--;°°°Mashinegun°°°
+				{priority = 1.8, class = UnitClass.AATank, unit = "sdkfz7_1"},--39
+				{priority = 1.8, class = UnitClass.AATank, unit = "sdkfz7_2a"},--39
+				{priority = 2.2, class = UnitClass.AATank, unit = "welberwind"},--44
+--;°°°Heavy tanks°°°
+				{priority = 2.0, class = UnitClass.Tank, unit = "fcm2c"},--39
+				{priority = 2.1, class = UnitClass.Tank, unit = "b1ter"},--40
+				{priority = 2.2, class = UnitClass.Tank, unit = "vk3601h1v2"},--41
+				{priority = 2.3, class = UnitClass.Tank, unit = "tiger_i_h1"},--41
+				{priority = 2.3, class = UnitClass.Tank, unit = "tiger_p"},--41
+				{priority = 2.4, class = UnitClass.Tank, unit = "tiger_i"},--42
+				{priority = 2.4, class = UnitClass.Tank, unit = "kt"},--42
+				{priority = 2.5, class = UnitClass.Tank, unit = "pz6bh"},--43
+				{priority = 2.6, class = UnitClass.Tank, unit = "kt_h_105mm"},--44
+				{priority = 2.6, class = UnitClass.Tank, unit = "maus"},--45
+				{priority = 2.7, class = UnitClass.Tank, unit = "e-75_88l100"},--46
+				{priority = 2.7, class = UnitClass.Tank, unit = "e-100b"},--46		
 			},
 			["usa"] = {--usa
-				--{priority = 2.0, class = UnitClass.Infantry, unit = "riflemans(usa)"},
-				{priority = 2.0, class = UnitClass.Infantry, unit = "home_guards(usa)"},
-				--{priority = 1.0, class = UnitClass.Infantry, unit = "smgs(usa)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "elites(usa)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "stormtroopers(usa)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "home_guards(usa)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "smgs(usa)"},
+				{priority = 3.0, class = UnitClass.Infantry, unit = "riflemans(usa)"},
+				{priority = 4.0, class = UnitClass.Infantry, unit = "elites(usa)"},
+				{priority = 5.0, class = UnitClass.Infantry, unit = "stormtroopers(usa)"},
+				{priority = 10.0, class = UnitClass.Tank, unit = "m24_chaffee"},				
 			},
 			["eng"] = {--eng
-				--{priority = 2.0, class = UnitClass.Infantry, unit = "riflemans(eng)"},
-				{priority = 2.0, class = UnitClass.Infantry, unit = "home_guards(eng)"},
-				--{priority = 1.0, class = UnitClass.Infantry, unit = "smgs(eng)"},
-				--{priority = 1.0, class = UnitClass.Infantry, unit = "elites(eng)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "stormtroopers(eng)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "home_guards(eng)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "smgs(eng)"},
+				{priority = 3.0, class = UnitClass.Infantry, unit = "riflemans(eng)"},
+				{priority = 4.0, class = UnitClass.Infantry, unit = "elites(eng)"},
+				{priority = 5.0, class = UnitClass.Infantry, unit = "stormtroopers(eng)"},
+				{priority = 10.0, class = UnitClass.Tank, unit = "m24_chaffee"},				
 			},
 			["jap"] = {--jap
-				--{priority = 2.0, class = UnitClass.Infantry, unit = "riflemans(jap)"},
-				{priority = 2.0, class = UnitClass.Infantry, unit = "home_guards(jap)"},
-				--{priority = 1.0, class = UnitClass.Infantry, unit = "smgs(jap)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "elites(jap)"},
-				{priority = 1.0, class = UnitClass.Infantry, unit = "stormtroopers(jap)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "home_guards(jap)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "smgs(jap)"},
+				{priority = 3.0, class = UnitClass.Infantry, unit = "riflemans(jap)"},
+				{priority = 4.0, class = UnitClass.Infantry, unit = "elites(jap)"},
+				{priority = 5.0, class = UnitClass.Infantry, unit = "stormtroopers(jap)"},
+				{priority = 10.0, class = UnitClass.Tank, unit = "m24_chaffee"},				
 			}
 		}
 	}
